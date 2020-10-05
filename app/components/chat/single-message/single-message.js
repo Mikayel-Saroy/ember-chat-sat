a03b5fdf29fc6de540d7c8523877be78b8976cbb
Mikayel Saroyan <mikayelsaroyan@Mikayels-MacBook-Pro.local>
2020-10-05 12:09:43 +0400

    Messages (Fix)

--- app/components/chat/single-message/single-message.js
+++ app/components/chat/single-message/single-message.js
@@ -4,7 +4,22 @@ import {action} from '@ember/object';
 import {tracked} from '@glimmer/tracking';
 
 export default class ChatSingleMessageSingleMessageComponent extends Component {
-  @tracked isMouseOver = true;
+  @tracked messageColor;
+  @tracked messageMark = "";
+
+  @action checkIfNewMessage() {
+    console.log(this.args.message.isNewMessage);
+    if (this.args.message.isNewMessage) {
+      this.messageColor = this.args.newColor;
+      this.messageMark = this.args.newMessageMark;
+      setTimeout(() => {
+        this.messageColor = this.args.defaultColor;
+        this.messageMark = "";
+      }, 7000);
+    } else {
+      this.messageColor = this.args.defaultColor;
+    }
+  }
 
   @action addLike() {
 
